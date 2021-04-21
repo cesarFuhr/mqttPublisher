66b75ce023b6f12587aeefdf49fad275809889e7
cesarFuhr <cesar.cara@protonmail.com>
2021-04-21 15:53:54 -0300

    added: concurrent publish

--- internal/app/command/notify_pids.go
+++ internal/app/command/notify_pids.go
@@ -1,12 +1,14 @@
 package command
 
 import (
+	"context"
 	"time"
 
 	"github.com/cesarFuhr/mqttPublisher/internal/domain/pid"
 )
 
 func NewPIDsHandler(id string, p pid.Publisher) NotifyPIDsHandler {
+
 	return NotifyPIDsHandler{
 		ID:  id,
 		Pub: p,
@@ -24,18 +26,33 @@ type PIDCommand struct {
 	At    time.Time
 }
 
-func (s *NotifyPIDsHandler) Handle(pids []PIDCommand) error {
-	var err error
+func (h *NotifyPIDsHandler) Handle(pids []PIDCommand) error {
+	results := make(chan error, len(pids))
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(10*time.Second))
+	defer func() {
+		cancel()
+		close(results)
+	}()
+
 	for _, v := range pids {
-		err = s.Pub.Publish(s.ID, pid.PID{
-			PID:   v.PID,
-			Value: v.Value,
-			At:    v.At,
-		})
-		if err != nil {
-			return err
-		}
+		go h.publishAsync(ctx, results, v)
+	}
+
+	var err error
+	for i := 0; i < len(pids); i++ {
+		err = <-results
+	}
+	if err != nil {
+		return err
 	}
 
 	return nil
+}
+
+func (s *NotifyPIDsHandler) publishAsync(ctx context.Context, res chan error, cmd PIDCommand) {
+	res <- s.Pub.Publish(s.ID, pid.PID{
+		PID:   cmd.PID,
+		Value: cmd.Value,
+		At:    cmd.At,
+	})
 }
